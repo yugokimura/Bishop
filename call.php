f626c968d7e9b3320b17e738c0f5d9da01ae8e9c
g-beacon-10_beacon <g-beacon-10_beacon@beacon-10-pro-web-002.ms.internal>
2016-04-11 15:21:13 +0900

    test

--- call.php
+++ call.php
@@ -1,15 +1,33 @@
 <?php
 
-// メッセージ受信
 $json_string = file_get_contents('php://input');
-$json_object = json_decode($json_string);
-$content = $json_object->result{0}->content;
-$text = $content->text;
-$from = $content->from;
-$message_id = $content->id;
-$content_type = $content->contentType;
+$jsonObj = json_decode($json_string);
+$to = $jsonObj->{"result"}[0]->{"content"}->{"from"};
 
-file_put_contents( "line.txt", $json_string);
+// テキストで返事をする場合
+$response_format_text = ['contentType'=>1,"toType"=>1,"text"=>"hello"];
+// 画像で返事をする場合
+//$response_format_image = ['contentType'=>2,"toType"=>1,'originalContentUrl'=>"画像URL","previewImageUrl"=>"サムネイル画像URL"];
+// 他にも色々ある
+// ....
+
+// toChannelとeventTypeは固定値なので、変更不要。
+$post_data = ["to"=>[$to],"toChannel"=>"1383378250","eventType"=>"138311608800106203","content"=>$response_format_text];
+
+$ch = curl_init("https://trialbot-api.line.me/v1/events");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json; charser=UTF-8',
+    'X-Line-ChannelID: 1463484500',
+    'X-Line-ChannelSecret: 8ae041e46f1c03d723d43117b7f240fc',
+    'X-Line-Trusted-User-With-ACL: uf5be8774e64f411ace3d15ee82aad49a'
+    ));
+$result = curl_exec($ch);
+curl_close($ch);
+
 
 
 ?>
